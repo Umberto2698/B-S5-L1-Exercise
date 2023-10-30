fef26431bb2abfe7b74fb7e29e0b43c3083f8e15
Umberto Zappulla <136740583+Umberto2698@users.noreply.github.com>
2023-10-30 15:49:23 +0100

    Displayed menu

--- src/main/java/Lezione16/Lezione16Application.java
+++ src/main/java/Lezione16/Lezione16Application.java
@@ -2,12 +2,20 @@ package Lezione16;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class Lezione16Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Lezione16Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Lezione16Application.class, args);
 
+        configurationClass();
+    }
+
+    public static void configurationClass() {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(Lezione16Application.class);
+        System.out.println(ctx.getBean("getMenu"));
+        ctx.close();
+    }
 }
